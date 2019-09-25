44bd2c88501d2ca535c36729dfc0793a6a1358b9
TranTrungNam <nam.trantrung@vietstarexpress.com>
2019-09-26 06:14:30 +0700

    add thread pool

---
+++ BlockingCollectionDemo2/ThreadPool/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadPool
+{
+    public partial class ProductContext : DbContext
+    {
+
+        public ProductContext() : base("name=MyDbConnectionString")
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
